6cb4d75722fa616e34842db8e9a398e1e2286373
misohiyoko <kodama.gentaro.899@s.kyushu-u.ac.jp>
2023-05-21 22:53:13 +0900

    stubs update

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import test
 p7 = Pin(7, Pin.OUT)
 a = test.test()
 while True:
-    print(9)  #p7(LED用のピン)をHIGHに
+    print(a.cst())  #p7(LED用のピン)をHIGHに
     sleep(1)        #1s待機 sleep()の中はs(秒)
     p7.value(0)  #p7(LED用のピン)をLOWに
     sleep(1)     #1s待機

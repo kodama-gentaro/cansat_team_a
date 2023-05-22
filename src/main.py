6d20654beec9a816c4c011fb595e49cf9aab8712
misohiyoko <kodama.gentaro.899@s.kyushu-u.ac.jp>
2023-05-22 10:20:03 +0900

    reset main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,5 @@
 from machine import Pin
 from utime import sleep
-import test
 
-p7 = Pin(7, Pin.OUT)
-a = test.test()
-while True:
-    print(a.cst())  #p7(LED用のピン)をHIGHに
-    sleep(1)        #1s待機 sleep()の中はs(秒)
-    p7.value(0)  #p7(LED用のピン)をLOWに
-    sleep(1)     #1s待機
+
+

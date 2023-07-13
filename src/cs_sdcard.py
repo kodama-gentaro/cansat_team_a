354d65740283960356c576da73c7bf157479b8cf
misohiyoko <43713959+misohiyoko@users.noreply.github.com>
2023-07-13 17:33:19 +0900

    motor

--- src/cs_sdcard.py
+++ src/cs_sdcard.py
@@ -10,21 +10,24 @@ SPI_MISO = 16
 SPI_BANDRATE = 32000000
 FILE_TITLE = "starrail"
 
-class SDcard:
-    def __init__(self):
-        i = 0
-        while i < 5:
-            try:
-                self.cs_pin = Pin(CS_PIN)
-                self.spi = SPI(SPI_ID,  sck=Pin(SPI_SCK), mosi=Pin(SPI_MOSI), miso=Pin(SPI_MISO))
-                self.sd = SDCard(self.spi , self.cs_pin, SPI_BANDRATE)
-                os.mount(self.sd, '/sd')
-            except Exception as e:
-                print("SDCARD_ERROR")
-                print(e)
-                sleep_ms(100)
-                i += 1
-                break
+
+def init(self):
+    i = 0
+    while i < 5:
+        try:
+            self.cs_pin = Pin(CS_PIN)
+            self.spi = SPI(SPI_ID,  sck=Pin(SPI_SCK), mosi=Pin(SPI_MOSI), miso=Pin(SPI_MISO))
+            self.sd = SDCard(self.spi , self.cs_pin, SPI_BANDRATE)
+            os.mount(self.sd, '/sd')
+        except Exception as e:
+            print("SDCARD_ERROR")
+            print(e)
+            sleep_ms(100)
+            i += 1
+            break
+
+def write(str):
+    
 
 
 
--- src/main.py
+++ src/main.py
@@ -7,12 +7,28 @@ import cs_nine_axis_sensor as imu
 
 md.init()
 imu.init()
-md.set_motor(1,1)
-print(imu.get_eulervalue())
 
+md.set_motor(0, 0)
 
-
-
-
-
-
+while True:
+    break
+    print(imu.get_eulervalue())
+    th1 = imu.get_eulervalue()[0]
+    th2 = 40
+    print(th1)
+    if th1 > th2:
+        if th1 - th2 < 5:
+            md.set_motor(1, 1)
+        else:
+            if th1 - th2 <= 180:
+                md.set_motor(-1, 1)
+            else:
+                md.set_motor(1, -1)
+    else:
+        if th2 - th1 < 5:
+            md.set_motor(1, 1)
+        else:
+            if th2 - th1 <= 180:
+                md.set_motor(1, -1)
+            else:
+                md.set_motor(-1, 1)

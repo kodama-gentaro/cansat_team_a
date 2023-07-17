c0bab243bcdc5e45089e90c3fe61ca385d1c6245
tanaka-nariyoshi <tanaka.nariyoshi.070@s.kyushu-u.ac.jp>
2023-07-17 16:37:57 +0900

    MOSFET またいじった

--- src/MOSFET.py
+++ src/MOSFET.py
@@ -1,7 +1,6 @@
 import machine
 import utime
 from machine import Pin
-import time
 
 IN1 = machine.Pin(10, machine.Pin.IN,Pin.PULL_DOWN)
 IN2 = machine.Pin(11,machine.Pin.OUT)
@@ -13,7 +12,7 @@ while True:
     print(IN1.value())
     if IN1.value() == 0:
         p7.value(1)
-        time.sleep(8)
+        utime.sleep(8)
         p7.value(0)
         break
     else:

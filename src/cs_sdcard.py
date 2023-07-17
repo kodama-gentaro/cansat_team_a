0591daa26deac999e039be999806a7dbafdc247e
misohiyoko <43713959+misohiyoko@users.noreply.github.com>
2023-07-17 17:55:55 +0900

    sd

--- src/cs_sdcard.py
+++ src/cs_sdcard.py
@@ -1,14 +1,15 @@
+from utime import ticks_ms
 from machine import Pin, SPI
 import os
-from sdcard import  SDCard
+from sdcard import SDCard
 from time import sleep_ms
-CS_PIN = 17  #回路作ってから多分変える
+
+CS_PIN = 17
 SPI_ID = 0
 SPI_SCK = 18
 SPI_MOSI = 19
 SPI_MISO = 16
 SPI_BANDRATE = 32000000
-FILE_TITLE = "starrail"
 
 
 def init(self):
@@ -16,8 +17,8 @@ def init(self):
     while i < 5:
         try:
             self.cs_pin = Pin(CS_PIN)
-            self.spi = SPI(SPI_ID,  sck=Pin(SPI_SCK), mosi=Pin(SPI_MOSI), miso=Pin(SPI_MISO))
-            self.sd = SDCard(self.spi , self.cs_pin, SPI_BANDRATE)
+            self.spi = SPI(SPI_ID, sck=Pin(SPI_SCK), mosi=Pin(SPI_MOSI), miso=Pin(SPI_MISO))
+            self.sd = SDCard(self.spi, self.cs_pin, SPI_BANDRATE)
             os.mount(self.sd, '/sd')
         except Exception as e:
             print("SDCARD_ERROR")
@@ -25,13 +26,17 @@ def init(self):
             sleep_ms(100)
             i += 1
             break
-
-def write(str):
-    
+    raise OSError
 
 
-
-
-
-
-
+def write(text):
+    i = 0
+    while i < 5:
+        try:
+            file = open(text(ticks_ms()) + ".txt", 'w')
+            file.write(text)
+            file.close()
+        except OSError:
+            print("SDCARD_ERROR")
+            i += 1
+    raise OSError

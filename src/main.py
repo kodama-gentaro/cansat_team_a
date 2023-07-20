0eb61300d05c40849fbbeca17fe52e3c0565e32d
Yoshihiro-Kikuchi <kikuchi.yoshihiro.607@s.kyushu-u.ac.jp>
2023-07-20 17:18:44 +0900

    Update main.py
    フライトピンのプログラムを追加しました

--- src/main.py
+++ src/main.py
@@ -6,6 +6,23 @@ import cs_motor_driver as md
 import cs_nine_axis_sensor as imu
 import math as m
 
+IN1 = machine.Pin(10, machine.Pin.IN,Pin.PULL_DOWN)
+IN2 = machine.Pin(11, machine.Pin.OUT)
+p7 = machine.Pin(7, machine.Pin.OUT)
+
+IN2.value(1)
+while True:
+    #print(IN1.value())
+    print(IN1.value())
+    if IN1.value() == 0 :
+        p7.value(1)
+        utime.sleep(8)
+        p7.value(0)
+        break
+    else :
+        print("A")
+    utime.sleep(0.5)
+
 md.init()
 imu.init()
 

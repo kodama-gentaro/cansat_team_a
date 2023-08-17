b490ff64baf797a22123f63fb31886455b147f5b
Yoshihiro-Kikuchi <kikuchi.yoshihiro.607@s.kyushu-u.ac.jp>
2023-08-17 11:21:36 +0900

    mos

--- src/MOSFET.py
+++ src/MOSFET.py
@@ -4,16 +4,16 @@ from machine import Pin
 
 IN1 = machine.Pin(10, machine.Pin.IN,Pin.PULL_DOWN)
 IN2 = machine.Pin(11,machine.Pin.OUT)
-p7 = machine.Pin(7, machine.Pin.OUT)
+p6 = machine.Pin(6, machine.Pin.OUT)
 
 IN2.value(1)
 
 while True:
     print(IN1.value())
     if IN1.value() == 0:
-        p7.value(1)
+        p6.value(1)
         utime.sleep(8)
-        p7.value(0)
+        p6.value(0)
         break
     else:
         print("A")
